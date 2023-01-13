d24fd9f0621d4e8d64ca7a7655cfa3431b2a1659
ivanvgav <61671242+ivanvgav@users.noreply.github.com>
2023-01-13 17:51:12 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gavriloff-toma-4-ng-proyect';
+}
