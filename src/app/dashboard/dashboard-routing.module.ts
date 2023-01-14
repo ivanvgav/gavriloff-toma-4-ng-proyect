d4f47f1c589833fcd3c8c2aba1035411dadcc31e
ivanvgav <61671242+ivanvgav@users.noreply.github.com>
2023-01-14 15:53:12 -0300

    Add Dashboard

---
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'students',
+        loadChildren: () => import('./students/students.module').then((module) => module.StudentsModule)
+      },
+      {
+        path: 'courses',
+        loadChildren: () => import('./courses/courses.module').then((module) => module.CoursesModule)
+      },
+      {
+        path: 'commissions',
+        loadChildren: () => import('./commisions/comissions.module').then((module) => module.ComissionsModule)
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      }
+    ]
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
