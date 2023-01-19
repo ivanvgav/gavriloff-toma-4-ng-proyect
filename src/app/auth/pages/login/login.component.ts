4c04174e83e2e0daf355ea64935c725793c84669
ivanvgav <61671242+ivanvgav@users.noreply.github.com>
2023-01-19 08:34:43 -0300

    Formating login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -8,38 +8,41 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-
 export class LoginComponent implements OnDestroy {
-  public loading = false
+  public loading = false;
   public form = new FormGroup({
     email: new FormControl('michael.lawson@reqres.in', [Validators.required]),
     password: new FormControl('cityslicka', [Validators.required]),
-  })
+  });
   private destroyed$ = new Subject();
 
   constructor(
     private readonly authService: AuthService,
     private readonly sessionService: SessionService,
-    private readonly router: Router,
+    private readonly router: Router
   ) {
-    this.sessionService.user$.pipe(takeUntil(this.destroyed$)).subscribe((user) => {
-      if (user) {
-        this.router.navigate(['dashboard', 'students'])
-      }
-    });
-   }
+    this.sessionService.user$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((user) => {
+        if (user) {
+          this.router.navigate(['dashboard', 'students']);
+        }
+      });
+  }
 
   ngOnDestroy(): void {
-    this.destroyed$.next(true)
+    this.destroyed$.next(true);
   }
 
   login() {
-    this.loading = true
-    this.authService.login({
-      email: this.form.get('email')?.value || '',
-      password: this.form.get('password')?.value || ''
-    }).subscribe(() => this.loading = false) 
+    this.loading = true;
+    this.authService
+      .login({
+        email: this.form.get('email')?.value || '',
+        password: this.form.get('password')?.value || '',
+      })
+      .subscribe(() => (this.loading = false));
   }
 }
